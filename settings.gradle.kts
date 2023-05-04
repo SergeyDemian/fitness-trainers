a66b072bb030d2e26897d4e050491a149e085d38
sergD <18104339@mail.ru>
2023-05-04 12:43:51 +0400

    add generator

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,16 +3,21 @@ rootProject.name = "fitness-trainers"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openapiVersion: String by settings
     val kotestVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
-        kotlin("multiplatform") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion apply false
         id("io.kotest.multiplatform") version kotestVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
 
 include(
     "home-work-1",
+    "fitness-trainers-api",
     "fitness-trainers-app"
 )
